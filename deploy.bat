c1b2d9c24e317f904745664ca3d54be146ff23a8
Jonas Adler <jonasadl@kth.se>
2018-09-20 09:45:08 +0200

    Change deploy order

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,7 @@
 echo "Commiting changes..."
 
-
-REM Push changes publicly
-git add -A
-git commit -m "%~1"
-git push origin master
-
 echo "Deploying updates to GitHub..."
 .\hugo
-
 
 REM Push changes publicly
 cd public
@@ -18,3 +11,8 @@ git push origin master
 
 REM  Come Back up to the Project Root
 cd ..
+
+REM Also push this repository
+git add -A
+git commit -m "%~1"
+git push origin master
